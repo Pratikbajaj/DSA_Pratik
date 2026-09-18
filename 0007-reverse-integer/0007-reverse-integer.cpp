3d414e49161750089c2f3eca6b14f662f47ec953
Pratikbajaj <118829104+Pratikbajaj@users.noreply.github.com>
2026-09-18 17:01:41 +0530

    Time: 0 ms (100%), Space: 8.4 MB (83.27%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,9 +5,11 @@ public:
         while(x!=0){
             long long int n=x%10;
             x=x/10;
-            if (sum > INT_MAX / 10 || (sum == INT_MAX / 10 && n > 7))
+            if (sum > 214748364 || sum < -214748364)
                 return 0;
-            if (sum < INT_MIN / 10 || (sum == INT_MIN / 10 && n < -8))
+            if (sum == 214748364 && n > 7)
+                return 0;
+            if (sum == -214748364 && n < -8)
                 return 0;
             sum=(sum*10)+n;
         }
