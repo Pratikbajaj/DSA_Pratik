3fe6f7888f2929ad677f3fafcdd94e922523b98b
Pratikbajaj <118829104+Pratikbajaj@users.noreply.github.com>
2026-09-22 11:39:46 +0530

    Time: 2 ms (15.2%), Space: 8.8 MB (88.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,45 +1,45 @@
-// class Solution {
-// public:
-//     bool isValid(string s) {
-//         int n=size(s);
-//         stack<char> s1;
-//         for(int i=0;i<n;i++){
-//             if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-//                 s1.push(s[i]);
-//             }
-//             else {
-//                 if(s1.empty()){
-//                     return false;
-//                 }
-//                 if((s[i]==')' && s1.top()=='(')||
-//                 (s[i]=='}' && s1.top()=='{')||
-//                 (s[i]==']' && s1.top()=='[')){
-//                     return false;
-//                 }
-//                 s1.pop();
-//             }
-//         }
-//     return s1.empty();
-//     }
-// };
 class Solution {
 public:
     bool isValid(string s) {
+        int n=size(s);
         stack<char> s1;
-        for (char c : s) {
-            if (c == '(' || c == '{' || c == '[') {
-                s1.push(c);
-            } else {
-                if (s1.empty()) return false;
-                char top = s1.top();
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                s1.push(s[i]);
+            }
+            else {
+                if(s1.empty()){
+                    return false;
+                }
+                if((s[i]==')' && s1.top()!='(')||
+                (s[i]=='}' && s1.top()!='{')||
+                (s[i]==']' && s1.top()!='[')){
                     return false;
                 }
                 s1.pop();
             }
         }
-        return s1.empty();
+    return s1.empty();
     }
 };
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> s1;
+//         for (char c : s) {
+//             if (c == '(' || c == '{' || c == '[') {
+//                 s1.push(c);
+//             } else {
+//                 if (s1.empty()) return false;
+//                 char top = s1.top();
+//                 if ((c == ')' && top != '(') ||
+//                     (c == '}' && top != '{') ||
+//                     (c == ']' && top != '[')) {
+//                     return false;
+//                 }
+//                 s1.pop();
+//             }
+//         }
+//         return s1.empty();
+//     }
+// };
